bbe8f434583e90d42bd21e3e838ed3e74915bd96
kimseonghwan <37522045+YeCheonHwa@users.noreply.github.com>
2021-09-29 13:47:34 +0900

    커밋테스트

---
+++ src/gitTest/main.java
@@ -0,0 +1,17 @@
+package gitTest;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("git branch test projcet");
+        String str = "git";
+
+        System.out.println(strFilter(str));
+    }
+
+    //
+    public static String strFilter(String str) {
+        String str1 = str;
+        return str1;
+    }
+
+}
